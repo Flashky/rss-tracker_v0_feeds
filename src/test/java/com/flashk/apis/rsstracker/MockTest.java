9774fd60d4259b5ff2eb60fff5e0e8655017784a
Flashky <borja.ruiz.contractor@bbva.com>
2021-04-30 10:32:21 +0200

    Add temporal test mock to allow Jacoco to generate a report for Codacy

---
+++ src/test/java/com/flashk/apis/rsstracker/MockTest.java
@@ -0,0 +1,14 @@
+package com.flashk.apis.rsstracker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MockTest {
+
+	@Test
+	void test() {
+		assertEquals(5, 2+3);
+	}
+
+}
