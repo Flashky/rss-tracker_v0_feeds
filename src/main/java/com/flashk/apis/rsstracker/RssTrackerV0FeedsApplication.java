299f941aee4b09fd92d2c4f6c900e8cb9ebe5089
Flashky <borja.ruiz.contractor@bbva.com>
2021-04-30 10:09:07 +0200

    Initial commit

---
+++ src/main/java/com/flashk/apis/rsstracker/RssTrackerV0FeedsApplication.java
@@ -0,0 +1,13 @@
+package com.flashk.apis.rsstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssTrackerV0FeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssTrackerV0FeedsApplication.class, args);
+	}
+
+}
