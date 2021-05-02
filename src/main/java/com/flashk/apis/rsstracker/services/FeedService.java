8a79fef15e85de8f5f5147e94f7cf468245b075b
Flashk <mevengar@gmail.com>
2021-05-02 18:40:59 +0200

    Implement internal service for getFeed

--- src/main/java/com/flashk/apis/rsstracker/services/FeedService.java
+++ src/main/java/com/flashk/apis/rsstracker/services/FeedService.java
@@ -1,11 +1,13 @@
 package com.flashk.apis.rsstracker.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.flashk.apis.rsstracker.controllers.model.Feed;
 
 public interface FeedService {
 
 	List<Feed> listFeeds();
+	Optional<Feed> getFeed(String feedId);
 	
 }
--- src/test/java/com/flashk/apis/rsstracker/services/FeedServiceImplTest.java
+++ src/test/java/com/flashk/apis/rsstracker/services/FeedServiceImplTest.java
@@ -1,10 +1,16 @@
 package com.flashk.apis.rsstracker.services;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,6 +32,7 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings("unchecked")
 class FeedServiceImplTest {
 
 	private static PodamFactory podamFactory;
@@ -53,7 +60,6 @@ class FeedServiceImplTest {
 
 
 	@Test
-	@SuppressWarnings("unchecked")
 	void testListFeeds() {
 		
 		// Prepare POJOs
@@ -66,10 +72,72 @@ class FeedServiceImplTest {
 		List<Feed> result = feedService.listFeeds();
 		
 		// Assertions
+		Mockito.verify(feedRepository).findAll(); // Verify it executed at least once.
+		
 		assertNotNull(result);
 		assertEquals(expected.size(), result.size());
 
 		// Mapping testing is performed at: FeedMapperTest
+	}
+	
+	@Test
+	void testListFeedsEmpty() {
+		
+		// Prepare POJOs
+		List<FeedEntity> expected = new ArrayList<>();
+		
+		// Prepare mocks
+		Mockito.doReturn(expected).when(feedRepository).findAll();
+		
+		// Execute method
+		List<Feed> result = feedService.listFeeds();
+		
+		// Assertions
+		Mockito.verify(feedRepository).findAll(); // Verify it executed at least once.
+		
+		assertNotNull(result);
+		assertEquals(expected.size(), result.size());
+		assertTrue(result.isEmpty());
+
+	}
+	
+	@Test
+	void testGetFeed() {
+		
+		// Prepare POJOs
+		FeedEntity expected = podamFactory.manufacturePojo(FeedEntity.class);
+		Optional<FeedEntity> expectedOptional = Optional.of(expected);
+		
+		// Prepare mocks
+		Mockito.doReturn(expectedOptional).when(feedRepository).findById(any());
+		
+		// Execute method
+		Optional<Feed> result = feedService.getFeed("1234");
+		
+		// Assertions
+		Mockito.verify(feedRepository).findById(any()); // Verify it executed at least once
+		
+		assertNotNull(result);
+		assertTrue(result.isPresent()); // There is data
+	}
+	
+	@Test
+	void testGetFeedNotPresent() {
+		
+		// Prepare POJOs
+		Optional<FeedEntity> expectedOptional = Optional.empty();
+		
+		// Prepare mocks
+		Mockito.doReturn(expectedOptional).when(feedRepository).findById(any());
+		
+		// Execute method
+		Optional<Feed> result = feedService.getFeed("1234");
+		
+		// Assertions
+		Mockito.verify(feedRepository).findById(any()); // Verify it executed at least once
+		
+		assertNotNull(result);
+		assertFalse(result.isPresent()); // There is data
 	}
 
 
