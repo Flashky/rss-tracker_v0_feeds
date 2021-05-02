c1c84b40713763264b70db3f38c9217e275b5cfb
Flashk <mevengar@gmail.com>
2021-05-02 19:24:29 +0200

    Implement getFeed controller and unit tests

--- src/main/java/com/flashk/apis/rsstracker/controllers/FeedRestController.java
+++ src/main/java/com/flashk/apis/rsstracker/controllers/FeedRestController.java
@@ -1,9 +1,13 @@
 package com.flashk.apis.rsstracker.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +28,17 @@ public class FeedRestController {
 		
 	}
 	
+	
+	@GetMapping("/{feedId}")
+	public ResponseEntity<Feed> getFeed(@PathVariable String feedId) {
+		
+		Optional<Feed> feed = feedService.getFeed(feedId);
+		
+		if(feed.isPresent()) {
+			return new ResponseEntity<Feed>(feed.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Feed>(HttpStatus.NO_CONTENT);
+		}
+		
+	}
 }
--- src/test/java/com/flashk/apis/rsstracker/controllers/FeedRestControllerTest.java
+++ src/test/java/com/flashk/apis/rsstracker/controllers/FeedRestControllerTest.java
@@ -1,10 +1,13 @@
 package com.flashk.apis.rsstracker.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.flashk.apis.rsstracker.controllers.model.Feed;
 import com.flashk.apis.rsstracker.services.FeedService;
@@ -22,6 +27,7 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings("unchecked")
 class FeedRestControllerTest {
 
 	@InjectMocks
@@ -46,7 +52,6 @@ class FeedRestControllerTest {
 
 	
 	@Test
-	@SuppressWarnings("unchecked")
 	void testListFeeds() {
 		
 		// Prepare POJOs
@@ -61,6 +66,45 @@ class FeedRestControllerTest {
 		// Assertions
 		assertNotNull(result);
 		assertEquals(expected.size(), result.size());
+
+	}
+	
+	@Test
+	void testGetFeed() {
+		
+		// Prepare POJOs
+		Feed expected = podamFactory.manufacturePojo(Feed.class);
+		Optional<Feed> expectedOptional = Optional.of(expected);
+		
+		// Prepare mocks
+		Mockito.doReturn(expectedOptional).when(feedService).getFeed(any());
+		
+		// Execute method
+		ResponseEntity<Feed> result = feedRestController.getFeed("1234");
+		
+		// Assertions
+		assertNotNull(result);
+		assertNotNull(result.getBody());
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+
+	}
+	
+	@Test
+	void testGetFeedNotPresent() {
+		
+		// Prepare POJOs
+		Optional<Feed> expectedOptional = Optional.empty();
+		
+		// Prepare mocks
+		Mockito.doReturn(expectedOptional).when(feedService).getFeed(any());
+		
+		// Execute method
+		ResponseEntity<Feed> result = feedRestController.getFeed("1234");
+		
+		// Assertions
+		assertNotNull(result);
+		assertNull(result.getBody());
+		assertEquals(HttpStatus.NO_CONTENT, result.getStatusCode());
 
 	}
 
