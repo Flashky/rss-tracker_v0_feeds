d7b36dd766fa21fd62f5276ea2abf51b82207002
Flashk <mevengar@gmail.com>
2021-10-28 14:24:53 +0200

    Remove unused import

--- src/main/java/com/flashk/apis/rsstracker/RssTrackerV0FeedsApplication.java
+++ src/main/java/com/flashk/apis/rsstracker/RssTrackerV0FeedsApplication.java
@@ -3,7 +3,6 @@ package com.flashk.apis.rsstracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@EnableMongoRepositories
